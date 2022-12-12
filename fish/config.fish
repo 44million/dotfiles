52e18d25c53b423ddafcf8e03e72d4d6a9eee332
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-12-12 12:56:54 -0700

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/brianli/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
